ac4329bd7a165425888e768ffb1815ac3bdaff8b
Yahe <hello@yahe.sh>
2018-10-17 19:36:14 +0200

    updated plugin to be compatible with Urlaube 0.1a7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.1a3 (17.10.2018)
+### Features
+* updated plugin to be compatible with Urlaube 0.1a7
+
 ## 0.1a2 (02.06.2018)
 ### Feature
 * rewrote to use the new translation mechanism of Urlaube 0.1a4
@@ -11,4 +15,3 @@
 ## 0.1a0 (26.05.2018)
 ### Features
 * initial version
-
--- plugin.php
+++ plugin.php
@@ -3,10 +3,11 @@
   /**
     This is the SearchWidget plugin.
 
-    This file contains the SearchWidget plugin. It provides a widget that prints a search form.
+    This file contains the SearchWidget plugin. It provides a widget that prints
+    a search form.
 
     @package urlaube\searchwidget
-    @version 0.1a2
+    @version 0.1a3
     @author  Yahe <hello@yahe.sh>
     @since   0.1a0
   */
@@ -16,36 +17,33 @@
   // prevent script from getting called directly
   if (!defined("URLAUBE")) { die(""); }
 
-  if (!class_exists("SearchWidget")) {
-    class SearchWidget extends Base implements Plugin {
+  class SearchWidget extends BaseSingleton implements Plugin {
 
-      // RUNTIME FUNCTIONS
+    // RUNTIME FUNCTIONS
 
-      public static function plugin() {
-        $result = new Content();
+    public static function plugin() {
+      $result = new Content();
 
-        $result->set(CONTENT, fhtml("<form action=\"%s\" id=\"searchwidget\" method=\"post\">".NL.
-                                    "  <div class=\"input-group\">".NL.
-                                    "    <input class=\"form-control\" name=\"search\" type=\"text\">".NL.
-                                    "      <span class=\"input-group-btn\">".NL.
-                                    "        <button class=\"btn btn-default\" type=\"submit\">".NL.
-                                    "          <span class=\"glyphicon glyphicon-search\"></span>".NL.
-                                    "        </button>".NL.
-                                    "      </span>".NL.
-                                    "  </div>".NL.
-                                    "</form>",
-                                    Main::ROOTURI()."search/"));
-        $result->set(TITLE,   t("Suche", "SearchWidget"));
+      $result->set(CONTENT, fhtml("<form action=\"%s\" id=\"searchwidget\" method=\"post\">".NL.
+                                  "  <div class=\"input-group\">".NL.
+                                  "    <input class=\"form-control\" name=\"search\" type=\"text\">".NL.
+                                  "      <span class=\"input-group-btn\">".NL.
+                                  "        <button class=\"btn btn-default\" type=\"submit\">".NL.
+                                  "          <span class=\"glyphicon glyphicon-search\"></span>".NL.
+                                  "        </button>".NL.
+                                  "      </span>".NL.
+                                  "  </div>".NL.
+                                  "</form>",
+                                  SearchHandler::getUriPost(new Content())));
+      $result->set(TITLE,   t("Suche", SearchWidget::class));
 
-        return $result;
-      }
-
+      return $result;
     }
 
-    // register plugin
-    Plugins::register("SearchWidget", "plugin", ON_WIDGETS);
-
-    // register translation
-    Translate::register(__DIR__.DS."lang".DS, "SearchWidget");
   }
 
+  // register plugin
+  Plugins::register(SearchWidget::class, "plugin", ON_WIDGETS);
+
+  // register translation
+  Translate::register(__DIR__.DS."lang".DS, SearchWidget::class);
